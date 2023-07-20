0c0e4caf2c6980e86b03985a3f6c7fadea72c71b
754118890@qq.com <754118890@qq.com>
2023-07-20 14:27:42 +0800

    完成商品列表的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,85 @@
+<template>
+  <view class="goods-item">
+    <!-- 商品左侧图片区域 -->
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
+    <!-- 商品右侧图片区域 -->
+    <view class="goods-item-right">
+      <!-- 商品名称 -->
+      <view class="goods-name">
+        {{goods.goods_name}}
+      </view>
+    
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="goods-price">
+        <!--  | 管道符 -->
+        ￥{{goods.goods_price | toFixed}}
+      </view>
+    </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-goods",
+    
+    // 外界传入属性
+    props:{
+      goods:{
+        type: Object,
+        default:{}
+      }
+    },
+    
+    data() {
+      return {
+        // 默认图片
+        defaultPic: 'https://alifei05.cfp.cn/creative/vcg/800/version23/VCG41157673045.jpg'
+        
+      };
+    },
+    
+    // 过滤器
+    filters:{
+      // 把数字处理为两位小数点的数字
+      toFixed(num){
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item{
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #efefef;
+  .goods-item-left{
+    margin-right: 5px;
+    .goods-pic{
+      height: 100px;
+      width: 100px;
+      display: block;
+    }
+  }
+  
+  .goods-item-right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 13px;
+    }
+    .goods-info-box{
+      .goods-price{
+         font-size: 16px;
+         color: #C00000;
+      }
+    }
+  }
+  
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,13 @@
 <template>
   <view>
-    doo
+    <view class="goods-list">
+      <view v-for="(goods,i) in  goodsList" :key="i" @click="gotoDetail(goods)">
+        <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+        <my-goods :goods="goods"></my-goods>
+      </view>
+      
+      
+    </view>
   </view>
 </template>
 
@@ -8,12 +15,91 @@
   export default {
     data() {
       return {
-        
+        // 定义请求参数对象
+        queryObj:{
+          // 查询关键词
+          query:'',
+          // 商品分类id
+          cid:'',
+          // 页码
+          pagenum:1,
+          // 每页显示多少条数据
+          pagesize:10
+        },
+        // 商品数据列表
+        goodsList:[],
+        // 总页数
+        total:0,
+        // 节流阀控制
+        isLoading:false
       };
+    },
+    
+    // 页面初始化加载
+    onLoad(options) {
+      this.queryObj.query=options.query || '',
+      this.queryObj.cid=options.cid || ''
+      
+      // 请求商品列表信息
+      this.getGoodsList()
+    },
+    
+    methods:{
+      // 请求商品类表信息 ,通过async和await优化异步请求结果
+      async getGoodsList(cb){
+        // 打开节流阀
+        this.isLoading=true
+        const {data:res}=await uni.$http.get('api/public/v1/goods/search',this.queryObj)
+        // 请求结束后，关闭节流阀
+        this.isLoading=false
+        // 如果外界传入cb, 则只要数据请求完毕，就立即按需调用 cb 回调函数
+        cb && cb()
+        
+        if(res.meta.status!==200) return uni.$showMsg()
+        // 新老数据合并
+        this.goodsList=[...this.goodsList,...res.message.goods]
+        this.total=res.message.total
+      },
+      
+      // 点击商品，跳转到对应商品的详情页
+      gotoDetail(item){
+        uni.navigateTo({
+          url:'/subpkg/goods_details/goods_details?goods_id='+item.goods_id
+        })
+      }
+      
+    },
+    
+    // 上拉触底，加载更多数据
+    onReachBottom() {
+      // 判断下一页是否还有数据
+      if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total) return uni.$showMsg('数据加载完毕!')
+      
+      // 先判断当前请求是否结束，防止额外的请求
+      if(this.isLoading) return
+      // 首先让请求页码+1
+      this.queryObj.pagenum++,
+      // 再重新请求数据
+      this.getGoodsList()
+    },
+    
+    // 下拉刷新时，需要重置数据
+    onPullDownRefresh() {
+      this.queryObj.pagenum=1
+      this.total=0
+      this.isLoading=false
+      this.goodsList=[]
+      
+      // 重新调用请求商品信息, 传入一个回调函数，请求结束后，自动调用关闭下拉刷新功能
+      this.getGoodsList(()=>{uni.stopPullDownRefresh()})
     }
+    
+    
+    
   }
 </script>
 
 <style lang="scss">
+
 
 </style>
