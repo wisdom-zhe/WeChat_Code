1fff9217353f6f7cafc350f165da85b5ef4770ea
754118890@qq.com <754118890@qq.com>
2023-07-24 12:08:25 +0800

    完成了商品详情页面的开发

--- subpkg/goods_details/goods_details.vue
+++ subpkg/goods_details/goods_details.vue
@@ -1,6 +1,43 @@
 <template>
-  <view>
-    goods_details
+  <view v-if="goods_info.goods_name" class="goods-detail-container">
+    <!-- 商品轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="(item,i) in goods_info.pics" :key="i">
+        <image :src="item.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    
+    <!-- 商品信息区域 -->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">￥{{goods_info.goods_price}}</view>
+      <!-- 信息主体区域 -->
+      <view class="goods-info-body">
+        <!-- 商品名称 -->
+        <view class="goods-name">{{goods_info.goods_name}}</view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <!-- 运费 -->
+      <view class="yf">快递：免运费</view>
+    </view>
+    
+    <!-- 渲染图片详情信息 -->
+    <rich-text :nodes="goods_info.goods_introduce"></rich-text>
+    
+    <!-- 商品导航组件 -->
+    <view class="goods_nav">
+      <!-- fill 控制右侧按钮的样式 -->
+      <!-- options 左侧按钮的配置项 -->
+      <!-- buttonGroup 右侧按钮的配置项 -->
+      <!-- click 左侧按钮的点击事件处理函数 -->
+      <!-- buttonClick 右侧按钮的点击事件处理函数 -->
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick" @buttonClick="buttonClick" />
+    </view>
+    
   </view>
 </template>
 
@@ -8,12 +45,133 @@
   export default {
     data() {
       return {
+        // 商品详情数据
+        goods_info:[],
         
+        // 左侧按钮组件的配置对象
+        options:[{
+          icon: 'shop',
+                text: '店铺'
+              }, {
+                icon: 'cart',
+                text: '购物车',
+                info: 2
+              }
+        ],
+        
+        //右侧按钮组的配置对象
+    buttonGroup: [{
+        text: '加入购物车',
+        backgroundColor: '#ff0000',
+        color: '#fff'
+      },
+      {
+        text: '立即购买',
+        backgroundColor: '#ffa200',
+        color: '#fff'
+      }
+    ]
       };
+    },
+    
+    // 页面初始化
+    onLoad(options) {
+      // 从请求页面中后去商品id
+      const goods_id=options.goods_id
+      // 获取商品详情数据
+      this.getGoodsDetail(goods_id)
+    },
+    
+    methods:{
+      async getGoodsDetail(goods_id){
+        const { data: res } = await uni.$http.get('api/public/v1/goods/detail', { goods_id })
+        if(res.meta.status!==200) return uni.$showMsg()
+        
+        // 对请求回来的商品详情做一些样式的处理
+        // 使用字符串的 replace() 方法，将 webp 的后缀名替换为 jpg 的后缀名
+        res.message.goods_introduce=res.message.goods_introduce.replace(/<img /g,'<img style="displack:block;"').replace(/webp/g,'jpg')
+        this.goods_info=res.message
+      },
+      
+      // 图片放大轮播
+      preview(i){
+        uni.previewImage({
+          // 预览时，默认显示图片的索引
+          current:i,
+          // 所有图片url地址的数组
+          urls:this.goods_info.pics.map(x=>x.pics_big)
+        })
+      },
+      
+      // 左侧按钮点击事件
+      onClick(e){
+        if(e.content.text==='购物车'){
+          uni.switchTab({
+            url:'/pages/cart/cart'
+          })
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss">
+swiper{
+  height: 750rpx;
+  image{
+    height: 100%;
+    width: 100%;
+  }
+}
+
+.goods-info-box{
+  padding: 10px;
+  padding-right: 0;
+  .price{
+    color: #c00000;
+    font-size: 18px;
+    margin: 10px 0;
+  }
+  .goods-info-body{
+    display: flex;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 13px;
+      padding-right: 10px;
+    }
+    .favi{
+      color:gray;
+      font-size: 12px;
+      width: 120px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border-left: 1px solid #efefef;
+    }
+  }
+  
+  .yf{
+    margin: 10px 0;
+    font-size: 12px;
+    color: gray;
+  }
+  
+  .goods-detail-container {
+    // 给页面外层的容器，添加 50px 的内padding，
+    // 防止页面内容被底部的商品导航组件遮盖
+    padding-bottom: 50px;
+  }
+  
+  .goods_nav{
+    // 为商品导航组件添加固定定位
+      position: fixed;
+      bottom: 0;
+      left:0;
+      width:100%
+  }
+  
+}
+
 
 </style>
